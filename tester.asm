b37fc6b5f2abaa7aa406ef0f5ef82fb74a0b932f
lily zhou <lilyxmzhou@gmail.com>
2024-03-30 16:21:20 -0400

    bottom collision

--- tester.asm
+++ tester.asm
@@ -18,24 +18,12 @@
 .text
 main: 
 
-	jal set_screen_BLUE
+	li $t0, FRAME_BASE
 	li $t5, 0x0096d9e9
 	sw $t5, 0($t0)
-	j END
 
-set_screen_BLUE:
-	li $t0, FRAME_BASE
-	li $t1, 0xff0000
-	addi $t2, $t0, 65024
-	j setPixel_SCREEN
 	
-setPixel_SCREEN:
-
-    sw $t1, 0($t0) # set $t2 to white
-    addi $t0, $t0, 512 # increment $t0 to next row
-    ble $t0, $t2, setPixel_SCREEN
-    jr $ra
-
+	lw $t1, 0($t0)
     
 END: 
 	li $v0, 10 # terminate the program gracefully
